6af1a23b7002678b1ba23eed04158db045729f9e
thschleicher <thschleicher@edu.aau.at>
2024-03-28 13:09:13 +0100

    refactor: join lobbyRequest to record

--- src/main/java/se2/group3/backend/dto/JoinLobbyRequest.java
+++ src/main/java/se2/group3/backend/dto/JoinLobbyRequest.java
@@ -3,13 +3,6 @@ package se2.group3.backend.dto;
 import lombok.Getter;
 
 @Getter
-public class JoinLobbyRequest {
+public record JoinLobbyRequest(long lobbyID, PlayerDTO player) {
 
-    private final long lobbyID;
-    private final String playerName;
-
-    public JoinLobbyRequest(long lobbyID, String playerName) {
-        this.lobbyID = lobbyID;
-        this.playerName = playerName;
-    }
 }
