8173cab7c1bdfab167b68c0fb2950e73c98a18d0
thschleicher <thschleicher@edu.aau.at>
2024-03-28 20:36:41 +0100

    feat: add isFull() to Lobby and player[] to store players associated with the lobby

--- src/main/java/se2/group3/backend/model/Lobby.java
+++ src/main/java/se2/group3/backend/model/Lobby.java
@@ -8,11 +8,14 @@ public class Lobby implements Runnable {
     private boolean running;
 
     private final long id;
+    private final Short MAX_PLAYERS = 4;
     private final Player host;
+    private final Player[] players = new Player[MAX_PLAYERS];
 
     public Lobby(long lobbyID, Player host) {
         this.id = lobbyID;
         this.host = host;
+        this.players[0] = host;
 
         //todo: host should be added automatically during creation
     }
@@ -30,10 +33,14 @@ public class Lobby implements Runnable {
         return true;
     }
 
-    public void removePlayer(Player player) {
+    public void removePlayer(String playerID) {
         // if number of players <= 0 the session should be closed
         //todo: remove player logic
         //todo: if player is host, move host to another player at random
+    }
+
+    public boolean isFull() {
+        return players.length >= MAX_PLAYERS;
     }
 
     public void closeLobby() {
