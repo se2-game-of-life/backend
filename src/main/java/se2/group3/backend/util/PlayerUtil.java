010d49227c8f2849f0b066381fb7d4433b398991
Johanna Klancnik <johannakl@edu.aau.at>
2024-04-03 21:05:06 +0200

    feat: add method createJSON() *this method takes a Player object and converts it into a JSON string *the JSON string can later be communicated to the app

--- src/main/java/se2/group3/backend/util/PlayerUtil.java
+++ src/main/java/se2/group3/backend/util/PlayerUtil.java
@@ -1,13 +1,30 @@
 package se2.group3.backend.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.exc.StreamWriteException;
+import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import se2.group3.backend.dto.PlayerDTO;
+
+import java.io.IOException;
 
 public class PlayerUtil {
     PlayerDTO player;
     ObjectMapper objectMapper;
     String player_JSON;
+
+    private void createJSON(){
+        objectMapper = new ObjectMapper();
+        try{
+            player_JSON = objectMapper.writeValueAsString(player);
+        } catch (StreamWriteException sw) {
+            sw.printStackTrace();
+        } catch (DatabindException db) {
+            db.printStackTrace();
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+    }
 
     private void messageReceivedFromApp(String message) {
         objectMapper = new ObjectMapper();
@@ -16,7 +33,6 @@ public class PlayerUtil {
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
-
     }
 
 }
