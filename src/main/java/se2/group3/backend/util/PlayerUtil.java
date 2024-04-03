c7faff35914af519bc77889feea7f8968a111c65
Johanna Klancnik <johannakl@edu.aau.at>
2024-04-03 21:03:15 +0200

    feat: add method messageReceivedFromApp() *this method reads the message sent by the app (JSON string) and converts it into a player object

--- src/main/java/se2/group3/backend/util/PlayerUtil.java
+++ src/main/java/se2/group3/backend/util/PlayerUtil.java
@@ -1,7 +1,22 @@
 package se2.group3.backend.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import se2.group3.backend.dto.PlayerDTO;
 
 public class PlayerUtil {
+    PlayerDTO player;
+    ObjectMapper objectMapper;
+    String player_JSON;
 
+    private void messageReceivedFromApp(String message) {
+        objectMapper = new ObjectMapper();
+        try {
+            player = objectMapper.readValue(message, PlayerDTO.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
