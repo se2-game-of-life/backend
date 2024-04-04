2a752802a656343c21267e854aaf96075fc93e09
Aya Saafan <ayasafan16@gmail.com>
2024-04-04 19:28:44 +0200

    feat: add CareerCard to Player

--- src/main/java/se2/group3/backend/model/Player.java
+++ src/main/java/se2/group3/backend/model/Player.java
@@ -1,10 +1,16 @@
 package se2.group3.backend.model;
 
+import se2.group3.backend.model.cards.CareerCard;
+
+import java.util.List;
+
 public class Player {
 
     private String playerName;
     private int currentCellPosition;
     private int money;
+
+    private CareerCard careerCard;
     private boolean isCollegePath; // Indicates if the player chose the college path
     private int investmentNumber; // The chosen investment number
     private int investmentLevel; // The current level of investment
@@ -40,6 +46,15 @@ public class Player {
             investmentLevel++; // Increase investment level
         }
     }
+
+    // TODO: Method to receive career cards and choose one
+    public void receiveCareerCards(List<CareerCard> careerCards) {
+        // Assuming CareerCard class is defined separately and extends Card
+        // Here, you'd receive the top two Career cards from the deck
+        // Let's assume you have access to the deck of Career cards
+        // Then you can deal the top two cards to the player
+    }
+
 
 
     // Getter and setter methods
