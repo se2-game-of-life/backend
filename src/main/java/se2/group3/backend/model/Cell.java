37e15ac09daaace5038851ae4b173cbe3c180b95
Aya Saafan <ayasafan16@gmail.com>
2024-04-04 17:18:55 +0200

    feat: Add Cell class

---
+++ src/main/java/se2/group3/backend/model/Cell.java
@@ -0,0 +1,25 @@
+package se2.group3.backend.model;
+
+import java.util.List;
+
+public abstract class Cell {
+    protected int position;
+    protected Cell nextCell;
+
+    public Cell(int position) {
+        this.position = position;
+    }
+
+    public Cell(int position, Cell nextCell) {
+        this.position = position;
+        this.nextCell = nextCell;
+    }
+
+    public void setNextCell(Cell nextCell) {
+        this.nextCell = nextCell;
+    }
+
+    public abstract void performAction(Player player);
+
+}
+
