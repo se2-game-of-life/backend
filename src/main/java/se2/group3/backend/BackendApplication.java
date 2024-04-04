2f25af130b11f4554bdee057ea2719bb7f7a54b0
Aya Saafan <ayasafan16@gmail.com>
2024-04-05 00:45:23 +0200

    Create a game instance

--- src/main/java/se2/group3/backend/BackendApplication.java
+++ src/main/java/se2/group3/backend/BackendApplication.java
@@ -2,12 +2,21 @@ package se2.group3.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import se2.group3.backend.model.Game;
 
 @SpringBootApplication
 public class BackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
+
+        ConfigurableApplicationContext context = SpringApplication.run(BackendApplication.class, args);
+
+        // Create a game instance
+        Game game = context.getBean(Game.class);
+
+        // Initialize decks
+        game.initializeDecks();
     }
 
 }
