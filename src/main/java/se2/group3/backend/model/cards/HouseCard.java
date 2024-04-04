8f905e1abf3ae38c242d76cbd2b5b7ff6b90af2b
Aya Saafan <ayasafan16@gmail.com>
2024-04-04 22:24:03 +0200

    extend HouseCard attributes

--- src/main/java/se2/group3/backend/model/cards/HouseCard.java
+++ src/main/java/se2/group3/backend/model/cards/HouseCard.java
@@ -4,8 +4,26 @@ import se2.group3.backend.model.Card;
 import se2.group3.backend.model.Player;
 
 public class HouseCard extends Card {
-    public HouseCard(String name) {
+    private int purchasePrice;
+    private int redSellPrice;
+    private int blackSellPrice;
+    public HouseCard(String name, int purchasePrice, int redSellPrice, int blackSellPrice) {
         super(name);
+        this.purchasePrice = purchasePrice;
+        this.redSellPrice = redSellPrice;
+        this.blackSellPrice = blackSellPrice;
+    }
+
+    public int getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public int getRedSellPrice() {
+        return redSellPrice;
+    }
+
+    public int getBlackSellPrice() {
+        return blackSellPrice;
     }
 
     @Override
