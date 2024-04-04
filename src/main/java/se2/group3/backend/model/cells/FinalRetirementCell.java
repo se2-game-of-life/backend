06e2eb0416a06de67c3ee734c84a119e836ea07e
Aya Saafan <ayasafan16@gmail.com>
2024-04-04 20:34:43 +0200

    feat: Add FinalRetirementCell class

---
+++ src/main/java/se2/group3/backend/model/cells/FinalRetirementCell.java
@@ -0,0 +1,17 @@
+package se2.group3.backend.model.cells;
+
+
+import se2.group3.backend.model.Cell;
+import se2.group3.backend.model.Player;
+
+// Cell for final retirement
+public class FinalRetirementCell extends Cell {
+    public FinalRetirementCell(int position) {
+        super(position);
+    }
+
+    @Override
+    public void performAction(Player player) {
+        // Implement logic for final retirement
+    }
+}
