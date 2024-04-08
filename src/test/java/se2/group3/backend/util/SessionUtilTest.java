498f69639f83f9e028afa50770a70e42e4172cf3
Thomas <thschleicher@edu.aau.at>
2024-04-08 10:08:54 +0200

    feat: add test class for sessionUtil

---
+++ src/test/java/se2/group3/backend/util/SessionUtilTest.java
@@ -0,0 +1,21 @@
+package se2.group3.backend.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionUtilTest {
+
+    @Test
+    void putSessionAttribute() {
+
+    }
+
+    @Test
+    void getLobbyID() {
+    }
+
+    @Test
+    void getUUID() {
+    }
+}
