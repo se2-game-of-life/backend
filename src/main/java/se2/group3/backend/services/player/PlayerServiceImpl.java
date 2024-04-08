bc44664a506f0bff24dce4beff506cf36f7a138e
Florian Hager <flo.hager12@gmail.com>
2024-04-08 13:42:16 +0200

    fix: fixed sonarcloud issues

--- src/main/java/se2/group3/backend/services/player/PlayerServiceImpl.java
+++ src/main/java/se2/group3/backend/services/player/PlayerServiceImpl.java
@@ -8,58 +8,53 @@ import se2.group3.backend.domain.cells.*;
 import se2.group3.backend.domain.player.Player;
 import se2.group3.backend.repositories.player.PlayerRepository;
 
-import java.util.Random;
+import java.security.SecureRandom;
 
 @Service
 public class PlayerServiceImpl implements PlayerService {
 
-    private final Integer INVESTMENTCOST = 50000;
+    private static final Integer INVESTMENT = 50000;
 
-    private final Integer INVESTMENTMULTIPLIER = 10000;
+    private final PlayerRepository repository;
 
     @Autowired
-    private PlayerRepository repository;
-
-
+    public PlayerServiceImpl(PlayerRepository repository) {
+        this.repository = repository;
+    }
 
     @Override
     public void chooseCollagePath(PlayerDTO dto) {
-        if(dto.isCollegePath()) {
-            if (repository.findById(dto.getPlayerID()).isPresent()) {
-                Player player = repository.findById(dto.getPlayerID()).get();
-                if (player.getMoney() > INVESTMENTCOST) {
-                    player.setMoney(player.getMoney() - INVESTMENTCOST);
-                    player.setCollegePath(true);
-                    repository.save(player);
-                }
+        if (dto.isCollegePath() && repository.findById(dto.getPlayerID()).isPresent()) {
+            Player player = repository.findById(dto.getPlayerID()).get();
+            if (player.getMoney() > INVESTMENT) {
+                player.setMoney(player.getMoney() - INVESTMENT);
+                player.setCollegePath(true);
+                repository.save(player);
             }
         }
     }
 
     @Override
     public void chooseMarryPath(PlayerDTO dto) {
-        if(dto.isMarriedPath()) {
-            if (repository.findById(dto.getPlayerID()).isPresent()) {
-                Player player = repository.findById(dto.getPlayerID()).get();
-                if (player.getMoney() > INVESTMENTCOST) {
-                    player.setMoney(player.getMoney() - INVESTMENTCOST);
-                    player.setMarriedPath(true);
-                    repository.save(player);
-                }
+        if (dto.isMarriedPath() && repository.findById(dto.getPlayerID()).isPresent()) {
+            Player player = repository.findById(dto.getPlayerID()).get();
+            if (player.getMoney() > INVESTMENT) {
+                player.setMoney(player.getMoney() - INVESTMENT);
+                player.setMarriedPath(true);
+                repository.save(player);
             }
+
         }
     }
 
     @Override
     public void chooseGrowFamilyPath(PlayerDTO dto) {
-        if(dto.isGrowFamiliePath()) {
-            if (repository.findById(dto.getPlayerID()).isPresent()) {
-                Player player = repository.findById(dto.getPlayerID()).get();
-                if (player.getMoney() > INVESTMENTCOST && dto.isGrowFamiliePath()) {
-                    player.setMoney(player.getMoney() - INVESTMENTCOST);
-                    player.setGrowFamiliePath(true);
-                    repository.save(player);
-                }
+        if (dto.isGrowFamiliePath() && repository.findById(dto.getPlayerID()).isPresent()) {
+            Player player = repository.findById(dto.getPlayerID()).get();
+            if (player.getMoney() > INVESTMENT && dto.isGrowFamiliePath()) {
+                player.setMoney(player.getMoney() - INVESTMENT);
+                player.setGrowFamiliePath(true);
+                repository.save(player);
             }
 
         }
@@ -67,25 +62,21 @@ public class PlayerServiceImpl implements PlayerService {
 
     @Override
     public void midLifeCrisisPath(PlayerDTO dto) {
-        if(dto.isHasMidlifeCrisis()) {
-            if (repository.findById(dto.getPlayerID()).isPresent()) {
-                Player player = repository.findById(dto.getPlayerID()).get();
-                player.setHasMidlifeCrisis(true);
-                repository.save(player);
-            }
+        if (dto.isHasMidlifeCrisis() && repository.findById(dto.getPlayerID()).isPresent()) {
+            Player player = repository.findById(dto.getPlayerID()).get();
+            player.setHasMidlifeCrisis(true);
+            repository.save(player);
         }
     }
 
     @Override
     public void chooseRetireEarlyPath(PlayerDTO dto) {
-        if(dto.isRetireEarlyPath()) {
-            if (repository.findById(dto.getPlayerID()).isPresent()) {
-                Player player = repository.findById(dto.getPlayerID()).get();
-                if (player.getMoney() > INVESTMENTCOST) {
-                    player.setMoney(player.getMoney() - INVESTMENTCOST);
-                    player.setRetireEarlyPath(true);
-                    repository.save(player);
-                }
+        if (dto.isRetireEarlyPath() && repository.findById(dto.getPlayerID()).isPresent()) {
+            Player player = repository.findById(dto.getPlayerID()).get();
+            if (player.getMoney() > INVESTMENT) {
+                player.setMoney(player.getMoney() - INVESTMENT);
+                player.setRetireEarlyPath(true);
+                repository.save(player);
             }
         }
     }
@@ -104,7 +95,7 @@ public class PlayerServiceImpl implements PlayerService {
         if (repository.findById(dto.getPlayerID()).isPresent()) {
             Player player = repository.findById(dto.getPlayerID()).get();
 
-            player.setMoney(player.getMoney() - INVESTMENTCOST);
+            player.setMoney(player.getMoney() - INVESTMENT);
             player.setInvestmentNumber(investmentNumber);
 
             repository.save(player);
@@ -117,13 +108,15 @@ public class PlayerServiceImpl implements PlayerService {
             Player player = repository.findById(dto.getPlayerID()).get();
 
             if (player.getInvestmentNumber() == spinResult) {
-                player.setMoney(player.getInvestmentLevel() * INVESTMENTMULTIPLIER);
+                int MULTIPLIER = 10000;
+                player.setMoney(player.getInvestmentLevel() * MULTIPLIER);
                 player.setInvestmentLevel(player.getInvestmentLevel() + 1);
                 repository.save(player);
             }
 
         }
     }
+
     @Override
     public void setOrUpdateCareer(PlayerDTO dto, CareerCard careerCard) {
         if (repository.findById(dto.getPlayerID()).isPresent()) {
@@ -136,20 +129,18 @@ public class PlayerServiceImpl implements PlayerService {
 
     @Override
     public void getPayOut(PlayerDTO dto, Cell passedCell) {
-        if (passedCell instanceof PaydayCell) {
-            if (repository.findById(dto.getPlayerID()).isPresent()) {
-                Player player = repository.findById(dto.getPlayerID()).get();
-                player.setMoney(player.getMoney() + player.getCareerCard().getSalary());
+        if (passedCell instanceof PaydayCell && repository.findById(dto.getPlayerID()).isPresent()) {
+            Player player = repository.findById(dto.getPlayerID()).get();
+            player.setMoney(player.getMoney() + player.getCareerCard().getSalary());
 
-                repository.save(player);
-            }
+            repository.save(player);
         }
     }
 
     @Override
     public void spin(PlayerDTO dto) {
-        Random spinResult = new Random();
-        if(repository.findById(dto.getPlayerID()).isPresent()) {
+        SecureRandom spinResult = new SecureRandom();
+        if (repository.findById(dto.getPlayerID()).isPresent()) {
             Player player = repository.findById(dto.getPlayerID()).get();
             player.setCurrentCellPosition(player.getCurrentCellPosition() + spinResult.nextInt(10));
         }
@@ -157,46 +148,50 @@ public class PlayerServiceImpl implements PlayerService {
 
     @Override
     public void checkCellAndPerformAction(PlayerDTO dto, Cell cell) {
-        if(repository.findById(dto.getPlayerID()).isPresent()) {
+        /*if (repository.findById(dto.getPlayerID()).isPresent()) {
             Player player = repository.findById(dto.getPlayerID()).get();
             //todo: implement the logic when a player steps on a cell
-            if(cell instanceof ActionCell) {
+            if (cell instanceof ActionCell) {
 
             }
-            if(cell instanceof AddPegCell) {
+            if (cell instanceof AddPegCell) {
 
             }
-            if(cell instanceof CareerCell) {
+            if (cell instanceof CareerCell) {
 
             }
-            if(cell instanceof FinalRetirementCell) {
+            if (cell instanceof FinalRetirementCell) {
 
             }
-            if(cell instanceof HouseCell) {
+            if (cell instanceof HouseCell) {
 
             }
-            if(cell instanceof InvestCell) {
+            if (cell instanceof InvestCell) {
 
             }
-            if(cell instanceof PaydayCell) {
+            if (cell instanceof PaydayCell) {
 
             }
         }
+        */
     }
 
     @Override
     public void checkStopCellAndPerformAction(PlayerDTO dto, StopCell stopCell) {
-        if(repository.findById(dto.getPlayerID()).isPresent()) {
+        /*
+        if (repository.findById(dto.getPlayerID()).isPresent()) {
             Player player = repository.findById(dto.getPlayerID()).get();
-            if(stopCell instanceof SpinToGraduateStopCell) {
+            if (stopCell instanceof SpinToGraduateStopCell) {
                 stopCell.performAction(player);
             }
-            if(stopCell instanceof GetMarriedStopCell) {
-               //todo: logic of function not yet implemented
+            if (stopCell instanceof GetMarriedStopCell) {
+                //todo: logic of function not yet implemented
                 stopCell.performAction(player);
             }
             //todo: rest of stop cells not yet implemented
         }
+
+         */
     }
 
 
