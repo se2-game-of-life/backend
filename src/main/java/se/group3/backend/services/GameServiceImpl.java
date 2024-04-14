1e19e8b0c60e0eaaaabd7be93eff79b8ebb04c4f
Johanna Klancnik <johannakl@edu.aau.at>
2024-04-14 16:21:36 +0200

    feat: add GameServiceImpl.java in which GameService.java will be implemented

--- src/main/java/se/group3/backend/services/GameServiceImpl.java
+++ src/main/java/se/group3/backend/services/GameServiceImpl.java
@@ -2,10 +2,32 @@ package se.group3.backend.services;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import se.group3.backend.DTOs.PlayerDTO;
+import se.group3.backend.dto.CellDTO;
+import se.group3.backend.dto.LobbyDTO;
 
 @Slf4j
 @Service
 public class GameServiceImpl implements GameService {
 
 
+    @Override
+    public void startGame() {
+        throw new UnsupportedOperationException("Not yet implemented.");
+    }
+
+    @Override
+    public void choosePath(PlayerDTO playerDTO) {
+        throw new UnsupportedOperationException("Not yet implemented.");
+    }
+
+    @Override
+    public void handleCell(PlayerDTO playerDTO, CellDTO cellDTO) {
+        throw new UnsupportedOperationException("Not yet implemented.");
+    }
+
+    @Override
+    public void getPlayerStats(LobbyDTO lobbyDTO) {
+        throw new UnsupportedOperationException("Not yet implemented.");
+    }
 }
