bab9c63d59ad5468d6e7aeb8188ed77b95b379b0
Johanna Klancnik <johannakl@edu.aau.at>
2024-04-16 14:39:31 +0200

    feat: add constructor and review comments in GameServiceImpl.java

--- src/main/java/se/group3/backend/services/GameServiceImpl.java
+++ src/main/java/se/group3/backend/services/GameServiceImpl.java
@@ -3,31 +3,74 @@ package se.group3.backend.services;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import se.group3.backend.DTOs.PlayerDTO;
+import se.group3.backend.domain.game.Game;
+import se.group3.backend.domain.lobby.Lobby;
 import se.group3.backend.dto.CellDTO;
 import se.group3.backend.dto.LobbyDTO;
+import se.group3.backend.repositories.ActionCardRepository;
+import se.group3.backend.repositories.CareerCardRepository;
+import se.group3.backend.repositories.HouseCardRepository;
 
+
+/**
+ * Implementation of the Interface GameService
+ */
 @Slf4j
 @Service
 public class GameServiceImpl implements GameService {
 
+    private Game game;
+
+    private CareerCardRepository careerCardRepository;
+    private ActionCardRepository actionCardRepository;
+    private HouseCardRepository houseCardRepository;
+    private CellService cellService;
+
+
+
+    public GameServiceImpl(Lobby lobby){
+        this.game = new Game(careerCardRepository, actionCardRepository, houseCardRepository, cellService);
+    }
 
     @Override
-    public void startGame() {
-        throw new UnsupportedOperationException("Not yet implemented.");
+    public void startGame(LobbyDTO lobbyDTO) {
+        throw new UnsupportedOperationException("Not implemented yet.");
     }
 
     @Override
     public void choosePath(PlayerDTO playerDTO) {
-        throw new UnsupportedOperationException("Not yet implemented.");
+        //review: should this be implemented in GameService or PlayerService?
+        throw new UnsupportedOperationException("Not implemented yet.");
     }
 
     @Override
     public void handleCell(PlayerDTO playerDTO, CellDTO cellDTO) {
-        throw new UnsupportedOperationException("Not yet implemented.");
+        //review: should this be implemented in GameService or PlayerService?
+        throw new UnsupportedOperationException("Not implemented yet.");
     }
 
     @Override
     public void getPlayerStats(LobbyDTO lobbyDTO) {
-        throw new UnsupportedOperationException("Not yet implemented.");
+        throw new UnsupportedOperationException("Not implemented yet.");
     }
+
+    @Override
+    public void checkWinCondition(PlayerDTO playerDTO) {
+        //review: not implemented yet, but method definition exists in Game.java --> move method?
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    @Override
+    public void spinWheel(PlayerDTO playerDTO) {
+        //review: implemented in Game.java AND in PlayerService.java --> move method?
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    @Override
+    public void nextPlayer(PlayerDTO playerDTO) {
+        //review: implemented in as nextTurn() in Game.java --> move method?
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+
 }
