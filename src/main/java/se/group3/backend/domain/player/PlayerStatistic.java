4cc4a2df4931fb3ba70870b77be44c2dde9df082
Johanna Klancnik <johannakl@edu.aau.at>
2024-04-17 21:38:01 +0200

    feat: implement PlayerStatistic.java *to create objects which show the playername, money, investmentNumber, numberOfPegs and (later) the numberOfHouses

--- src/main/java/se/group3/backend/domain/player/PlayerStatistic.java
+++ src/main/java/se/group3/backend/domain/player/PlayerStatistic.java
@@ -1,4 +1,27 @@
 package se.group3.backend.domain.player;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
 public class PlayerStatistic {
+    private String playerName;
+    private int money;
+    private int investmentNumber;
+    private int numberOfPegs;
+    private int numberOfHouses;
+
+
+    public PlayerStatistic(Player player) {
+        this.playerName = player.getPlayerName();
+        this.money = player.getMoney();
+        this.investmentNumber = player.getInvestmentNumber();
+        this.numberOfPegs = player.getNumberOfPegs();
+        //todo: player needs a variable how many houses he has
+        this.numberOfHouses = 0;
+    }
+
 }
