9fa64c59c67e4b764b41abb1b24d4d2d47828225
nzhurat <zuratnasta2606@icloud.com>
2024-04-25 17:05:23 +0200

    bug fixing in HouseCard

--- src/main/java/se/group3/backend/domain/cards/HouseCard.java
+++ src/main/java/se/group3/backend/domain/cards/HouseCard.java
@@ -1,5 +1,6 @@
 package se.group3.backend.domain.cards;
 
+import se.group3.backend.domain.game.Deck;
 import se.group3.backend.domain.player.Player;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -8,6 +9,7 @@ public class HouseCard extends Card {
     private int purchasePrice;
     private int redSellPrice;
     private int blackSellPrice;
+
     public HouseCard(String name, int purchasePrice, int redSellPrice, int blackSellPrice) {
         super(name);
         this.purchasePrice = purchasePrice;
@@ -28,43 +30,21 @@ public class HouseCard extends Card {
     }
 
     @Override
-    public void performAction(Player player, Deck<HouseCard> deck) {
-        HouseCard card1 = deck.drawCard();
-        HouseCard card2 = deck.drawCard();
-
-        System.out.println("Choose a house to buy:");
-        System.out.println("1. " + card1);
-        System.out.println("2. " + card2);
-
-        HouseCard chosenCard = card1;
-        HouseCard unchosenCard = card2;
-
-        if (player.getMoney() >= chosenCard.getPurchasePrice()) {
-            player.setMoney(player.getMoney() - chosenCard.getPurchasePrice());
-
-            player.assignHouseCard(chosenCard);
-
-            System.out.println("Congratulations! You have bought the house: " + chosenCard.getName());
+    public void performAction(Player player) {
+        if (player.getMoney() >= purchasePrice) {
+            player.setMoney(player.getMoney() - purchasePrice);
+            player.assignHouseCard(this);
+            System.out.println("Congratulations! You have bought the house: " + getName());
         } else {
             System.out.println("Sorry, you don't have enough money to buy this house.");
         }
-
-        deck.addCard(unchosenCard);
     }
 
     public void sellHouse(Player player, boolean isRedOnWheel) {
-        int sellPrice = isRedOnWheel ? getRedSellPrice() : getBlackSellPrice();
-        
-        int index = -1;
-        for (int i = 0; i < player.getOwnedHouses().size(); i++) {
-            if (player.getOwnedHouses().get(i).equals(this)) {
-                index = i;
-                break;
-            }
-        }
+        int sellPrice = isRedOnWheel ? redSellPrice : blackSellPrice;
 
-        if (index != -1) {
-            player.getOwnedHouses().remove(index);
+        if (player.getOwnedHouses().contains(this)) {
+            player.getOwnedHouses().remove(this);
             player.setMoney(player.getMoney() + sellPrice);
             System.out.println("You have sold the house: " + getName() + " for " + sellPrice);
         } else {
@@ -72,12 +52,12 @@ public class HouseCard extends Card {
         }
     }
 
-
     @Override
     public String toString() {
         return "HouseCard{" +
                 "id='" + getId() + '\'' +
-                "name='" + getName() + '\'' +
+                ", name='" + getName() + '\'' +
                 '}';
     }
 }
+
