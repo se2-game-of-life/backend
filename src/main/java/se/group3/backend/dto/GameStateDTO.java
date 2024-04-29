9eaf0d604a1701fb04f76ed87233a8176c93d180
Florian <flo.hager12@gmail.com>
2024-04-29 17:31:45 +0200

    feat: added GameStateDTO.java to transfer the updated state of the game

---
+++ src/main/java/se/group3/backend/dto/GameStateDTO.java
@@ -0,0 +1,25 @@
+package se.group3.backend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import se.group3.backend.domain.cards.Card;
+import se.group3.backend.domain.player.Player;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class GameStateDTO {
+
+    /**
+     * UUID from player which has to take the turn
+     */
+    private String playerTurn;
+
+    private List<Player> players;
+
+    private int diceResult;
+
+    private List<Card> cardOptions;
+
+}
