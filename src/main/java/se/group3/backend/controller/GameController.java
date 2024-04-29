a3929e2b09e41219d7fbf9ed81c55ea9f82366e2
Thomas Schleicher <thschleicher@edu.aau.at>
2024-04-29 17:22:21 +0200

    feat: add outline for GameController.java

--- src/main/java/se/group3/backend/controller/GameController.java
+++ src/main/java/se/group3/backend/controller/GameController.java
@@ -1,4 +1,45 @@
 package se.group3.backend.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import se.group3.backend.services.GameService;
+import se.group3.backend.services.LobbyService;
+
+@Slf4j
+@Controller
 public class GameController {
+
+    private final GameService gameService;
+    private final LobbyService lobbyService;
+    private final SimpMessagingTemplate template;
+
+    @Autowired
+    public GameController(GameService gameService, LobbyService lobbyService, SimpMessagingTemplate template) {
+        this.gameService = gameService;
+        this.lobbyService = lobbyService;
+        this.template = template;
+    }
+
+    @MessageMapping("/game/start")
+    public void startGame() {
+        //get lobby and player id from session header
+    }
+
+    @MessageMapping("/game/path")
+    public void choosePath() {
+        //get lobby and player id from session header
+    }
+
+    @MessageMapping("/game/action")
+    public void chooseAction() {
+        //get lobby and player id from session header
+    }
+
+    @MessageMapping("/game/move")
+    public void handleMove() {
+        //get lobby and player id from session header
+    }
 }
