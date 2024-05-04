d0da1978af707b785976e228c28032f7b8daaacd
Florian <flo.hager12@gmail.com>
2024-05-04 18:50:49 +0200

    test: tested the refactored PlayerService for

--- src/test/java/se/group3/backend/service/PlayerServiceImplTest.java
+++ src/test/java/se/group3/backend/service/PlayerServiceImplTest.java
@@ -49,213 +49,46 @@ class PlayerServiceImplTest {
     }
 
     @Test
-    void chooseCollegePath_withEnoughMoney_updatesPlayer() {
-        dto.setCollegePath(true);
-
-        service.chooseCollagePath(dto);
-
-        verify(repository, atLeastOnce()).save(player);
-        assertTrue(player.isCollegePath());
-        assertEquals(200000, player.getMoney());
-    }
-
-    @Test
-    void chooseCollegePathFalse() {
-        dto.setCollegePath(false);
-
-        service.chooseCollagePath(dto);
-
-        verify(repository, atLeastOnce()).save(player);
-        assertFalse(player.isCollegePath());
-    }
-
-    @Test
-    void chooseMarriedPath_withEnoughMoney_updatesPlayer() {
-        dto.setMarriedPath(true);
-
-        service.chooseMarryPath(dto);
-
-        verify(repository, atLeastOnce()).save(player);
-        assertTrue(player.isMarriedPath());
-        assertEquals(200000, player.getMoney());
-    }
-
-    @Test
-    void chooseMarriedPathFalse() {
-        dto.setMarriedPath(false);
-
-        service.chooseMarryPath(dto);
-
-        verify(repository, atLeastOnce()).save(player);
-        assertFalse(player.isMarriedPath());
-    }
-
-    @Test
-    void chooseGrowFamilyPath_withEnoughMoney_updatesPlayer() {
-        dto.setGrowFamilyPath(true);
-
-        service.chooseGrowFamilyPath(dto);
-
-        verify(repository, atLeastOnce()).save(player);
-        assertTrue(player.isGrowFamilyPath());
-        assertEquals(200000, player.getMoney());
-    }
-
-    @Test
-    void chooseGrowFamilyPathFalse() {
-        dto.setGrowFamilyPath(false);
-
-        service.chooseGrowFamilyPath(dto);
-
-        verify(repository, atLeastOnce()).save(player);
-        assertFalse(player.isGrowFamilyPath());
-    }
-
-    @Test
-    void midLifeCrisisPath_setsFlag() {
-        dto.setHasMidlifeCrisis(true);
-
-        service.midLifeCrisisPath(dto);
-
-        verify(repository, atLeastOnce()).save(player);
-        assertTrue(player.isHasMidlifeCrisis());
-    }
-
-    @Test
-    void midLifeCrisisPath_setsFlagFalse() {
-        dto.setHasMidlifeCrisis(false);
-
-        service.midLifeCrisisPath(dto);
-
-        verify(repository, atLeastOnce()).save(player);
-        assertFalse(player.isHasMidlifeCrisis());
-    }
-
-    @Test
-    void chooseRetireEarlyPath_withEnoughMoney_updatesPlayer() {
-        dto.setRetireEarlyPath(true);
-
-        service.chooseRetireEarlyPath(dto);
-
-        verify(repository, atLeastOnce()).save(player);
-        assertTrue(player.isRetireEarlyPath());
-        assertEquals(200000, player.getMoney());
-    }
-
-    @Test
-    void chooseRetireEarlyPathFalse() {
-        dto.setRetireEarlyPath(false);
-
-        service.chooseRetireEarlyPath(dto);
-
-        verify(repository, atLeastOnce()).save(player);
-        assertFalse(player.isRetireEarlyPath());
-    }
-
-    @Test
     void increaseNumberOfPegs_increasesPegs() {
         int initialPegs = player.getNumberOfPegs();
 
-        service.increaseNumberOfPegs(dto);
+        service.increaseNumberOfPegs(dto.getPlayerID());
 
         verify(repository, atLeastOnce()).save(player);
         assertEquals(initialPegs + 1, player.getNumberOfPegs());
     }
-    @Test
-    void spin_increasesCurrentCellPosition() {
-        int initialPosition = player.getCurrentCellPosition();
-
-        service.spin(dto);
-
-        assertTrue(player.getCurrentCellPosition() >= initialPosition);
-    }
-    /*
-    Remove investment
-    @Test
-    void collectInvestmentPayout_withMatchingSpinResult_increasesMoney() {
-        player.setInvestmentNumber(5);
-        player.setInvestmentLevel(1);
-
-        service.collectInvestmentPayout(dto, 5);
-
-        verify(repository, atLeastOnce()).save(player);
-        assertEquals(10000, player.getMoney());
-        assertEquals(2, player.getInvestmentLevel());
-    }
 
     @Test
-    void invest_decreasesMoneyAndSetsInvestmentNumber() {
-        Integer investmentNumber = 5;
-
-        service.invest(dto, investmentNumber);
-
-        verify(repository, atLeastOnce()).save(player);
-        assertEquals(200000, player.getMoney());
-        assertEquals(investmentNumber, player.getInvestmentNumber());
-    }*/
-
-
-
-    @Test
-    void getPayOut_withPaydayCell_increasesMoneyBySalary() {
-        Cell paydayCell = new Cell(5, "paydayCell", Arrays.asList(6, 7), 6, 7); // Position und mögliche nächste Zellen
+    void getSalary_withPaydayCell_increasesMoneyBySalary() {
         CareerCard careerCard = new CareerCard("Doctor", 150000, 20000, false);
         player.setCareerCard(careerCard);
         int initialMoney = player.getMoney();
 
-        service.getPayOut(dto, paydayCell);
+        service.getSalary(dto.getPlayerID());
 
         verify(repository, atLeastOnce()).save(player);
         assertEquals(initialMoney + careerCard.getSalary(), player.getMoney());
     }
 
     @Test
-    void setOrUpdateCareer_withExistingPlayer_updatesCareerCard() {
+    void getSalary_withBonus() {
+        CareerCard careerCard = new CareerCard("Doctor", 150000, 20000, false);
+        player.setCareerCard(careerCard);
+        int initialMoney = player.getMoney();
+
+        service.getSalaryWithBonus(dto.getPlayerID());
+
+        verify(repository, atLeastOnce()).save(player);
+        assertEquals(initialMoney + careerCard.getBonus(), player.getMoney());
+    }
+
+    @Test
+    void setCareer_withExistingPlayer_updatesCareerCard() {
         CareerCard newCareerCard = new CareerCard("Engineer", 100000, 15000, false);
         when(repository.findById(dto.getPlayerID())).thenReturn(Optional.of(player));
-        service.setOrUpdateCareer(dto, newCareerCard);
+        service.setCareer(dto.getPlayerID(), newCareerCard);
 
         verify(repository, times(2)).save(player);
         Assertions.assertEquals(newCareerCard, player.getCareerCard());
     }
-
-    /*
-    Removed because cell subclass is removed. There is no cel.preform action anymore.
-    Cells shouldn't be tested in the player service tests
-
-    @Test
-    void checkCellAndPerformAction_StopCellPresent() {
-        PlayerDTO dto = new PlayerDTO();
-        dto.setPlayerID("TestID");
-        Player player = new Player("player1");
-        Cell cell1 = mock(Cell.class);
-        StopCell stopCell = mock(StopCell.class);
-        Cell cell3 = mock(Cell.class);
-        List<Cell> cells = Arrays.asList(cell1, stopCell, cell3);
-
-        when(repository.findById(dto.getPlayerID())).thenReturn(Optional.of(player));
-
-        service.checkCellAndPerformAction(dto, cells);
-
-        verify(cell1).performAction(player);
-        verify(stopCell).performAction(player);
-        verify(cell3, never()).performAction(player); // Verifies action stops after StopCell
-    }
-
-    @Test
-    void checkCellAndPerformAction_PlayerNotFound() {
-        // Setup
-        PlayerDTO dto = new PlayerDTO();
-        dto.setPlayerID("TestID");
-        Cell cell = mock(Cell.class);
-        List<Cell> cells = Collections.singletonList(cell);
-
-        when(repository.findById(dto.getPlayerID())).thenReturn(Optional.empty());
-
-        // Execution
-        service.checkCellAndPerformAction(dto, cells);
-
-        // Verification
-        verify(cell, never()).performAction(any(Player.class));
-    } */
 }
