94b65675d87c960c6c5de656f965916f394c0ccf
Johanna Klancnik <johannakl@edu.aau.at>
2024-05-06 20:11:14 +0200

    feat: add variables and constructor to GameController.java

--- src/main/java/se/group3/backend/controller/GameController.java
+++ src/main/java/se/group3/backend/controller/GameController.java
@@ -1,4 +1,31 @@
 package se.group3.backend.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import se.group3.backend.services.BoardService;
+import se.group3.backend.services.GameService;
+import se.group3.backend.services.LobbyService;
+
+
+@Slf4j
+@Controller
 public class GameController {
+    private final BoardService boardService;
+    private final SimpMessagingTemplate template;
+    private final GameService gameService;
+    private final LobbyService lobbyService;
+    private static final String ERROR_PATH = "/topic/errors/";
+    private static final String GAME_PATH = "/topic/game/";
+    @Autowired
+    public GameController(GameService gameService, LobbyService lobbyService, BoardService boardService, SimpMessagingTemplate template) {
+        this.boardService = boardService;
+        this.gameService = gameService;
+        this.lobbyService = lobbyService;
+        this.template = template;
+    }
+
+
+
 }
