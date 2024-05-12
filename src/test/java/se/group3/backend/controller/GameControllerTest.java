6356115a8ce6a84d61b9fdad261732d5a6153296
Johanna Klancnik <johannakl@edu.aau.at>
2024-05-12 20:36:26 +0200

    feat: add setUp() and breakDown() in GameControllerTest.java

--- src/test/java/se/group3/backend/controller/GameControllerTest.java
+++ src/test/java/se/group3/backend/controller/GameControllerTest.java
@@ -1,4 +1,28 @@
 package se.group3.backend.controller;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import se.group3.backend.services.GameService;
+
+
 public class GameControllerTest {
+    private GameController gameController;
+    private SimpMessagingTemplate messagingTemplate;
+    private GameService gameService;
+
+    @BeforeEach
+    void setUp() {
+        gameService = Mockito.mock(GameService.class);
+        messagingTemplate = Mockito.mock(SimpMessagingTemplate.class);
+        gameController = new GameController(gameService, null, null, messagingTemplate);
+    }
+
+    @AfterEach
+    void breakDown(){
+        gameService = null;
+        messagingTemplate = null;
+        gameController =null;
+    }
 }
