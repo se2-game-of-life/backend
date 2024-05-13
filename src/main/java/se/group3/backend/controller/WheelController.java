2f8aa5be4016d8b4974cafda874efd5502594472
nzhurat <zuratnasta2606@icloud.com>
2024-05-13 16:11:47 +0200

    add variables to WheelController

--- src/main/java/se/group3/backend/controller/WheelController.java
+++ src/main/java/se/group3/backend/controller/WheelController.java
@@ -1,4 +1,11 @@
 package se.group3.backend.controller;
 
+import se.group3.backend.domain.game.Game;
+
 public class WheelController {
+    private final Game game;
+
+    public WheelController(Game game) {
+        this.game = game;
+    }
 }
