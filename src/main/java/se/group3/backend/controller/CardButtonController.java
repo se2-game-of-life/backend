d48e83f16f2ce3816bc6cda393503436d56eb522
nzhurat <zuratnasta2606@icloud.com>
2024-05-13 15:29:15 +0200

    receive button click information

--- src/main/java/se/group3/backend/controller/CardButtonController.java
+++ src/main/java/se/group3/backend/controller/CardButtonController.java
@@ -1,4 +1,15 @@
 package se.group3.backend.controller;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class CardButtonController {
+
+    @MessageMapping("/app/buttonClicked")
+    public void handleButtonClicked(@Payload String buttonInfo) {
+        System.out.println("Button clicked: " + buttonInfo);
+    }
 }
+
