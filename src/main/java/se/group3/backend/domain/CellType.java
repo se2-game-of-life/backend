b8d082d01842c3aecbf1ff60b59e845bf50e6cb9
Johanna Klancnik <johannakl@edu.aau.at>
2024-05-17 19:49:19 +0200

    feat: add stop sign types to enum CellType.java

--- src/main/java/se/group3/backend/domain/CellType.java
+++ src/main/java/se/group3/backend/domain/CellType.java
@@ -1,5 +1,5 @@
 package se.group3.backend.domain;
 
 public enum CellType {
-    CASH, HOUSE, FAMILY, CAREER, ACTION
+    CASH, HOUSE, FAMILY, CAREER, ACTION, GRADUATE, MARRY, GROW_FAMILY, MID_LIFE, RETIRE_EARLY
 }
