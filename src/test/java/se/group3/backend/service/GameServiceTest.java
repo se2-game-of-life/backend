c8a802ad665ddb581f674c4a2baf41fbaa0a3c38
Johanna Klancnik <johannakl@edu.aau.at>
2024-05-22 11:01:26 +0200

    ref: rename GameServiceImplTest.java to GameServiceTest.java

--- src/test/java/se/group3/backend/service/GameServiceTest.java
+++ src/test/java/se/group3/backend/service/GameServiceTest.java
@@ -1,30 +1,24 @@
 package se.group3.backend.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import se.group3.backend.domain.Player;
 import se.group3.backend.dto.LobbyDTO;
 import se.group3.backend.dto.PlayerDTO;
 import se.group3.backend.repositories.PlayerRepository;
 import se.group3.backend.services.GameService;
-import se.group3.backend.services.SerializationService;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class GameServiceImplTest {
+class GameServiceTest {
     private PlayerDTO playerDTOMock;
     private final List<PlayerDTO> playersMock = new ArrayList<>(2);
     private LobbyDTO lobbyDTOMock;
