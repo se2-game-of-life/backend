1f76f86f86eb52be4ac487575ed9c8962f3dbf3d
Johanna Klancnik <johannakl@edu.aau.at>
2024-05-24 16:31:15 +0200

    feat: add parameterized test for case CAREER

--- src/test/java/se/group3/backend/service/GameServiceTest.java
+++ src/test/java/se/group3/backend/service/GameServiceTest.java
@@ -5,12 +5,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.junit.jupiter.MockitoExtension;
 import se.group3.backend.domain.Cell;
 import se.group3.backend.domain.CellType;
 import se.group3.backend.domain.Lobby;
 import se.group3.backend.domain.Player;
+import se.group3.backend.domain.cards.CareerCard;
 import se.group3.backend.domain.cards.HouseCard;
 import se.group3.backend.dto.LobbyDTO;
 import se.group3.backend.dto.PlayerDTO;
@@ -22,6 +25,7 @@ import se.group3.backend.services.GameService;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -38,6 +42,8 @@ class GameServiceTest {
 
     private final HouseCard houseCard1 = new HouseCard("House1", 50000,0,0);
     private final HouseCard houseCard2 = new HouseCard("House2", 10000,0,0);
+    private final static CareerCard careerCard1 = new CareerCard("Career1", 100, 200, false);
+    private final static CareerCard careerCard2 = new CareerCard("Career2", 100, 200, false);
 
 
     @BeforeEach
@@ -217,6 +223,39 @@ class GameServiceTest {
         assertEquals(240000, player.getMoney());
         assertEquals(List.of(houseCard2), player.getHouses());
     }
+
+    @ParameterizedTest
+    @MethodSource("testMakeChoiceCAREER_Input")
+    void testMakeChoice_Career(boolean chooseLeft, CareerCard card){
+        Player player = new Player();
+        player.setCurrentCellPosition(0);
+        player.setMoney(250000);
+        player.setLobbyID(1L);
+        player.setPlayerUUID("UUID");
+        when(playerRepository.findById("UUID")).thenReturn(Optional.of(player));
+
+        Lobby lobby = new Lobby(1L, player);
+        lobby.setCurrentPlayer(player);
+        lobby.setCards(List.of(careerCard1, careerCard2));
+        when(lobbyRepository.findById(player.getLobbyID())).thenReturn(Optional.of(lobby));
+
+        Cell startCell = mock(Cell.class);
+        when(startCell.getType()).thenReturn(CellType.CAREER);
+        when(cellRepository.findByNumber(player.getCurrentCellPosition())).thenReturn(startCell);
+
+        gameService.makeChoice(chooseLeft, "UUID");
+        assertEquals(card, player.getCareerCard());
+    }
+
+    static Stream<Arguments> testMakeChoiceCAREER_Input() {
+        return Stream.of(
+                Arguments.of(true, careerCard1),
+                Arguments.of(false, careerCard2)
+        );
+    }
+
+
+
 
 
 
