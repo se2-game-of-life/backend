61243a84d129903dca99bfd4d12168d1ac9bb9b3
Johanna Klancnik <johannakl@edu.aau.at>
2024-05-28 13:36:57 +0200

    ref: remove unused import statement

--- src/main/java/se/group3/backend/services/GameService.java
+++ src/main/java/se/group3/backend/services/GameService.java
@@ -7,7 +7,6 @@ import se.group3.backend.domain.Cell;
 import se.group3.backend.domain.CellType;
 import se.group3.backend.domain.Lobby;
 import se.group3.backend.domain.Player;
-import se.group3.backend.domain.cards.ActionCard;
 import se.group3.backend.domain.cards.Card;
 import se.group3.backend.domain.cards.CareerCard;
 import se.group3.backend.domain.cards.HouseCard;
