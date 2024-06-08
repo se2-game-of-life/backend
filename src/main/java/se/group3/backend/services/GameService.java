2931f3578a938a43673fdbb53290b5e8617b15ba
Johanna Klancnik <johannakl@edu.aau.at>
2024-06-08 16:32:32 +0200

    feat: add random career cards to lobby

--- src/main/java/se/group3/backend/services/GameService.java
+++ src/main/java/se/group3/backend/services/GameService.java
@@ -226,6 +226,7 @@ public class GameService {
                         }
                     }
                 }
+                lobby.setCards(careerCards);
                 lobby.setHasDecision(true);
                 break;
             case MID_LIFE:
