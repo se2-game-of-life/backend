3b0e4cf067cddc30c6f378a32b87412d0ba24656
Johanna Klancnik <johannakl@edu.aau.at>
2024-06-12 18:27:30 +0200

    fix: change query to aggregation and add method to find career card without diploma

--- src/main/java/se/group3/backend/repositories/HouseCardRepository.java
+++ src/main/java/se/group3/backend/repositories/HouseCardRepository.java
@@ -1,5 +1,6 @@
 package se.group3.backend.repositories;
 
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import se.group3.backend.domain.Player;
@@ -13,7 +14,7 @@ import java.util.List;
 public interface HouseCardRepository extends MongoRepository<HouseCard, String> {
     // Add custom query methods if needed
 
-    @Query(value = "{'$sample': {'size': 1}}")
+    @Aggregation(pipeline = { "{$sample: {size: 1}}" })
     HouseCard findRandomHouseCard();
 
     default List<Card> searchAffordableHousesForPlayer(int availableMoney){
@@ -21,7 +22,7 @@ public interface HouseCardRepository extends MongoRepository<HouseCard, String> 
         List<HouseCard> houses = findAll();
 
         for(HouseCard house : houses){
-            if(house.getPurchasePrice() <= availableMoney){
+            if(house.getPurchasePrice() <= availableMoney && affordableHouses.size() <= 2){
                 affordableHouses.add(house);
             }
         }
