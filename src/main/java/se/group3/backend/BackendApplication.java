1e30aa7e5f0f014793b1f2e6fcec5b311eed859b
Florian <flo.hager12@gmail.com>
2024-06-14 12:05:39 +0200

    fix: code warnings

--- src/main/java/se/group3/backend/BackendApplication.java
+++ src/main/java/se/group3/backend/BackendApplication.java
@@ -3,7 +3,6 @@ package se.group3.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 
 @SpringBootApplication
--- src/main/java/se/group3/backend/exceptions/NoUUIDException.java
+++ src/main/java/se/group3/backend/exceptions/NoUUIDException.java
@@ -1,13 +1,6 @@
 package se.group3.backend.exceptions;
 
 public class NoUUIDException extends Exception {
-    public NoUUIDException(String message) {
-        super(message);
-    }
-
-    public NoUUIDException(String message, Throwable cause) {
-        super(message, cause);
-    }
 
     public NoUUIDException(Throwable cause) {
         super(cause);
