d979452ebb723fd203a7b6151f4f8ffb6f83effc
Florian <flo.hager12@gmail.com>
2024-06-14 12:13:12 +0200

    fix: code warnings

--- src/main/java/se/group3/backend/repositories/HouseCardRepository.java
+++ src/main/java/se/group3/backend/repositories/HouseCardRepository.java
@@ -2,17 +2,12 @@ package se.group3.backend.repositories;
 
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import se.group3.backend.domain.cards.Card;
 import se.group3.backend.domain.cards.HouseCard;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public interface HouseCardRepository extends MongoRepository<HouseCard, String> {
-    // Add custom query methods if needed
-
-    @Aggregation(pipeline = { "{$sample: {size: 1}}" })
-    HouseCard findRandomHouseCard();
 
     default List<HouseCard> searchAffordableHousesForPlayer(int availableMoney){
         List<HouseCard> affordableHouses= new ArrayList<>();
