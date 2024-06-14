5563f0cb7e56286a1586075c7d68f4f5e6862f0a
nzhurat <zuratnasta2606@icloud.com>
2024-06-14 13:12:06 +0200

    add test testMakeChoice_Teleport_false

--- src/test/java/se/group3/backend/service/GameServiceTest.java
+++ src/test/java/se/group3/backend/service/GameServiceTest.java
@@ -212,6 +212,19 @@ class GameServiceTest {
         // Assuming the player moves 2 cells forward
         assertEquals(player.getCurrentCellPosition(), teleportCell.getNextCells().get(0) + 2);
     }
+    @Test
+    void testMakeChoice_Teleport_false() {
+        Cell teleportCell = mock(Cell.class);
+
+        when(playerRepository.findById("UUID")).thenReturn(Optional.of(player));
+        when(lobbyRepository.findById(player.getLobbyID())).thenReturn(Optional.of(lobby));
+        when(teleportCell.getType()).thenReturn(CellType.TELEPORT);
+        when(cellRepository.findByNumber(player.getCurrentCellPosition())).thenReturn(teleportCell);
+
+        gameService.makeChoice(false, "UUID");
+        // Assuming the player stays in the same place
+        assertEquals(player.getCurrentCellPosition(), teleportCell.getNextCells().get(0));
+    }
 
     @ParameterizedTest
     @MethodSource("testMakeChoiceCAREER_Input")
