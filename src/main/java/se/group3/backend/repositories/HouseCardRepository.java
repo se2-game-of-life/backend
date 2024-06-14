dafa937eddcbbc88f347917ce3ef563cb9f524f4
Florian <flo.hager12@gmail.com>
2024-06-14 12:13:31 +0200

    fix: unused import

--- src/main/java/se/group3/backend/repositories/HouseCardRepository.java
+++ src/main/java/se/group3/backend/repositories/HouseCardRepository.java
@@ -1,6 +1,5 @@
 package se.group3.backend.repositories;
 
-import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import se.group3.backend.domain.cards.HouseCard;
 
