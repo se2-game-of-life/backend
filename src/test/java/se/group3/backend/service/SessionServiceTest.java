da44578c5b1cd9c1a6f806bff4d3383ef270cb5b
Johanna Klancnik <johannakl@edu.aau.at>
2024-06-25 19:06:18 +0200

    ref: improve code coverage *add testcases for SessionService

--- src/test/java/se/group3/backend/service/SessionServiceTest.java
+++ src/test/java/se/group3/backend/service/SessionServiceTest.java
@@ -1,9 +1,7 @@
 package se.group3.backend.service;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import se.group3.backend.services.SessionService;
@@ -11,16 +9,19 @@ import se.group3.backend.services.SessionService;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
 class SessionServiceTest {
 
     private SimpMessageHeaderAccessor headerAccessor;
 
-    @InjectMocks
     private SessionService sessionService;
 
     @BeforeEach
     void setUp() {
         headerAccessor = Mockito.mock(SimpMessageHeaderAccessor.class);
+        sessionService = new SessionService();
     }
 
 
@@ -28,13 +29,61 @@ class SessionServiceTest {
     void getUUIDMissing() {
         Map<String, Object> sessionAttributes = new HashMap<>();
         sessionAttributes.put("uuid", null);
-        Mockito.when(headerAccessor.getSessionAttributes()).thenReturn(sessionAttributes);
-        Assertions.assertThrows(NullPointerException.class, () -> sessionService.getUUID(headerAccessor));
+        when(headerAccessor.getSessionAttributes()).thenReturn(sessionAttributes);
+        Exception e = assertThrows(Exception.class, () -> sessionService.getUUID(headerAccessor));
+
+        assertEquals("Session attribute 'uuid' is not of type String or missing from session header!", e.getMessage());
     }
 
     @Test
     void getUUIDMissingHeader() {
-        Mockito.when(headerAccessor.getSessionAttributes()).thenReturn(null);
-        Assertions.assertThrows(NullPointerException.class, () -> sessionService.getUUID(headerAccessor));
+        when(headerAccessor.getSessionAttributes()).thenReturn(null);
+        Exception e = assertThrows(Exception.class, () -> sessionService.getUUID(headerAccessor));
+
+        assertEquals("SessionAttributes could not be extracted from session header!", e.getMessage());
     }
+
+    @Test
+    void putUUID_headerMissing_test() {
+        when(headerAccessor.getSessionAttributes()).thenReturn(null);
+        String uuid = "123";
+
+        Exception e = assertThrows(Exception.class, () -> sessionService.putUUID(headerAccessor, uuid));
+        assertEquals("SessionAttributes could not be extracted from session header!", e.getMessage());
+    }
+
+
+    @Test
+    void getUUID_noString_test() {
+        Map<String, Object> sessionAttributes = new HashMap<>();
+        sessionAttributes.put("uuid", 1);
+        when(headerAccessor.getSessionAttributes()).thenReturn(sessionAttributes);
+
+        Exception e = assertThrows(Exception.class, () -> sessionService.getUUID(headerAccessor));
+        assertEquals("Session attribute 'uuid' is not of type String or missing from session header!", e.getMessage());
+    }
+
+    @Test
+    void putUUID_successfully_test(){
+        Map<String, Object> sessionAttributes = new HashMap<>();
+        when(headerAccessor.getSessionAttributes()).thenReturn(sessionAttributes);
+
+        String uuid = "123";
+        assertDoesNotThrow(() -> sessionService.putUUID(headerAccessor, uuid));
+
+        assertEquals(uuid, sessionAttributes.get("uuid"));
+    }
+
+    @Test
+    void getUUID_successfully_test(){
+        String uuid = "123";
+        Map<String, Object> sessionAttributes = new HashMap<>();
+        sessionAttributes.put("uuid", uuid);
+        when(headerAccessor.getSessionAttributes()).thenReturn(sessionAttributes);
+
+        String result = assertDoesNotThrow(()->sessionService.getUUID(headerAccessor));
+
+        assertEquals(uuid, result);
+    }
+
 }
